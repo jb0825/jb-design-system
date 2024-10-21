1b63bf543a45869f84113ee7183c9ddd24e5f5b2
jb0825 <jebi0825@naver.com>
2024-10-21 16:56:32 +0900

    fix: storybook 스토리 정렬 제거
    - Story 파일명으로 정렬하던 코드 주석 처리

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -20,11 +20,12 @@ const preview: Preview = {
       },
     },
     options: {
+      // 스토리북 정렬
       // The `a` and `b` arguments in this function have a type of `import('@storybook/types').IndexEntry`. Remember that the function is executed in a JavaScript environment, so use JSDoc for IntelliSense to introspect it.
-      storySort: (a, b) =>
-        a.id === b.id
-          ? 0
-          : a.id.localeCompare(b.id, undefined, { numeric: true }),
+      // storySort: (a, b) =>
+      //   a.id === b.id
+      //     ? 0
+      //     : a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
   },
 };
